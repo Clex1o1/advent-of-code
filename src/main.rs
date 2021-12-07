1606a68bbb74f511d1e06cda5347820c4ce6f9f6
Alexander Classen <ac@westwerk.ac>
2021-12-08 00:37:56 +0100

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod mod_init;
-pub use crate::mod_init::init;
+use crate::mod_init::init;
+mod mod_mesurements;
+use crate::mod_mesurements::mesurements;
 
 fn main() {
     // get entries from file
     let mesurements = init::get_mesurements();
     // loop through array and print each entry
+    day1("day1", &mesurements);
+    day2("day2", &mesurements);
+}
+
+fn day1(label: &str, mesurements: &Vec<i32>) {
     let mut last_entry: &i32 = &mesurements[0];
     let mut counter: i32 = 0;
     mesurements.iter().for_each(|entry| {
@@ -13,6 +20,10 @@ fn main() {
         }
         last_entry = entry
     });
-    println!("{} measurements are larger", counter);
-    println!("{} measurements total", mesurements.len());
+    println!("{}: {} measurements are larger", label, counter);
+}
+
+fn day2(label: &str, measurements: &Vec<i32>) {
+    let sum_chunks = mesurements::chunk_sum_measurements(measurements);
+    day1(label, &sum_chunks);
 }
--- src/mod_mesurements.rs
+++ src/mod_mesurements.rs
@@ -1,1 +1,17 @@
-pub mod mesurements {}
+pub mod mesurements {
+    pub fn chunk_sum_measurements(mesurements: &Vec<i32>) -> Vec<i32> {
+        let chunk_size = 3;
+        let chunk_step = 1;
+        let mut chunk_index = 0;
+        let mut chunks = Vec::new();
+        mesurements.iter().for_each(|_measurement| {
+            let chunk = mesurements.iter().skip(chunk_index).take(chunk_size);
+            if chunk.len() == chunk_size {
+                let sum = chunk.sum::<i32>();
+                chunks.push(sum);
+            }
+            chunk_index += chunk_step;
+        });
+        chunks
+    }
+}
